5a9086132db0c86a15954f9defc833752b7644fa
Jonathan Chua <chuabacca@gmail.com>
2021-04-13 22:25:03 -0700

    Initial Commit

---
+++ ChowList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChowList
+//
+//  Created by Jon on 4/13/21.
+//  Copyright © 2021 Jonathan Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
