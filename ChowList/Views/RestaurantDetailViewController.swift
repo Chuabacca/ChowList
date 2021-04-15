6c66af5bf021f7948af4352d48e1e162b942e2f3
Jonathan Chua <chuabacca@gmail.com>
2021-04-14 21:50:08 -0700

    Add MapKit view for restaurants.

--- ChowList/Views/RestaurantDetailViewController.swift
+++ ChowList/Views/RestaurantDetailViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantDetailViewController: UIViewController {
     var model: RestaurantsViewModel.Restaurant
 
     let nameLabel = UILabel()
+    let mapView = MKMapView()
     let addressLabel = UILabel()
 
     init(model: RestaurantsViewModel.Restaurant) {
@@ -39,6 +41,20 @@ class RestaurantDetailViewController: UIViewController {
         nameLabel.text = model.name
         view.addSubview(nameLabel)
 
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        let center = CLLocationCoordinate2D(latitude: model.latitude, longitude: model.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+        let pin = MKPointAnnotation()
+        pin.coordinate = center
+        mapView.addAnnotation(pin)
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.layer.cornerRadius = 10
+        mapView.clipsToBounds = true
+        view.addSubview(mapView)
+
         addressLabel.translatesAutoresizingMaskIntoConstraints = false
         addressLabel.numberOfLines = 0
         addressLabel.font = .systemFont(ofSize: 14)
@@ -50,8 +66,13 @@ class RestaurantDetailViewController: UIViewController {
             nameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
 
+            mapView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+
             addressLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            addressLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20)
+            addressLabel.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 20),
+            addressLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
     }
 }
